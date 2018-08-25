628decf9250b6b588eddab3ab4d0eb3aa4a8ceb5
Glitch (alike-charger) <none>
2018-08-25 02:02:45 +0000

    🕶🚣 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,54 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// Timestamp API
+// Handles null case, then UTC and unix cases, then invalid case
+app.get("/api/timestamp/:date_string", function(req,res) {
+  let date = req.params.date_string;
+  
+  if (!date) {
+    date = new Date();
+    res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
+  } else if (new Date(date) != 'Invalid Date' || new Date(date * 1000) != 'Invalid Date') {
+    if(new Date(date) != 'Invalid Date') {
+      date = new Date(date);
+      res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
+    } else {
+      date = date * 1000;
+      date = new Date(date);
+      res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
+    }
+  } else {
+    res.json({"unix": null, "utc" : "Invalid Date" });
+  }            
+})
